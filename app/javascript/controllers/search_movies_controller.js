a20b36c2861b019bea98576aba3e6e54463504e0
Ezequiel <epuyosdacosta@gmail.com>
2023-01-27 16:20:42 +0100

    Search on keyup

--- app/javascript/controllers/search_movies_controller.js
+++ app/javascript/controllers/search_movies_controller.js
@@ -5,12 +5,20 @@ export default class extends Controller {
   static targets = ["form", "input", "list"];
 
   connect() {
-    console.log(this.formTarget);
-    console.log(this.inputTarget);
-    console.log(this.listTarget);
+    // console.log(this.formTarget);
+    // console.log(this.formTarget.action);
+    // console.log(this.inputTarget);
+    // console.log(this.inputTarget.value);
+    // console.log(this.listTarget);
   }
 
   update() {
-    console.log("TODO update list")
+    // console.log("TODO update list")
+    const url = `${this.formTarget.action}?query=${this.inputTarget.value}`;
+    fetch(url, {headers: {"Accept": "text/plain"}})
+      .then(response => response.text())
+      .then((data) => {
+        this.listTarget.outerHTML = data
+      })
   }
 }
